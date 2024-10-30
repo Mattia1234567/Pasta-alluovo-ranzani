7954c5709cac34835d4794c6557f5e1b69f79f6b
Mattia1234567 <59102515+Mattia1234567@users.noreply.github.com>
2024-10-30 21:00:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// script.js
 
-// Funzione per mostrare un messaggio quando si clicca su un prodotto
-document.querySelectorAll('.product-item').forEach(item => {
-    item.addEventListener('click', () => {
-        alert('Funzionalità in arrivo! Puoi inserire qui le foto dei prodotti.');
-    });
-});
